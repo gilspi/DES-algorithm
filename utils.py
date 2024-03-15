65f51f371892b4b954507f4a43a38e3f5bfa132c
gilspi <gilspi@email.con>
2024-03-15 23:06:57 +0300

    Changed func dec2bin/bin2dec and added validate to bin2hex

--- utils.py
+++ utils.py
@@ -1,41 +1,24 @@
-def left_shift(binary: list, shift_amount: int):
+def shift_func(binary: list, shift_amount: int):
     shift_amount = shift_amount % len(binary)
-    return binary[shift_amount:] + binary[0: shift_amount]
-
-
-def right_shift(lst: list, steps: int):
-    if steps < 0:
-        steps = abs(steps)
-        for i in range(steps):
-            lst.append(lst.pop(0))
-    else:
-        for i in range(steps):
-            lst.insert(0, lst.pop())
-    return lst
+    return binary[shift_amount:] + binary[:shift_amount]
 
 
 # Binary to decimal conversion
-def bin2dec(binary):
-    binary1 = binary
-    decimal, i, n = 0, 0, 0
-    while binary != 0:
-        dec = binary % 10
-        decimal = decimal + dec * pow(2, i)
-        binary = binary//10
-        i += 1
-    return decimal
+def bin2dec(binary_number: str):
+    if not isinstance(binary_number, str):
+        raise TypeError("Входной аргумент должен быть строкой")
+
+    decimal_representation = int(binary_number, 2)
+
+    return decimal_representation
 
 
-# Decimal to binary conversion
-def dec2bin(num):
-    res = bin(num).replace("0b", "")
-    if len(res) % 4 != 0:
-        div = len(res) / 4
-        div = int(div)
-        counter = (4 * (div + 1)) - len(res)
-        for i in range(0, counter):
-            res = '0' + res
-    return res
+def dec2bin(decimal_number: int, n=4):
+    if not isinstance(decimal_number, int):
+        raise TypeError("Входной аргумент должен быть целым числом")
+
+    binary_representation = bin(decimal_number)[2:].zfill(n)
+    return binary_representation
 
 
 def xor(data_expanded: list, round_binary_keys: list):
@@ -46,18 +29,23 @@ def xor(data_expanded: list, round_binary_keys: list):
             xor_values.append(0)
         else:
             xor_values.append(1)
-
     return xor_values
 
 
 # Метод для преобразования блока из бинарного представления в шестнадцатеричное представление
-def bin2hex(binary_data: list, scale: int = 2):
-    binary_str = ''.join(map(str, binary_data))
+def bin2hex(data, scale: int = 2, is_needed: bool = False, n: int = 14):
+    if isinstance(data, list):
+        binary_str = ''.join(map(str, data))
+    elif isinstance(data, str):
+        binary_str = data
+    else:
+        raise TypeError("Данные должны быть либо списком, либо строкой")
+
     hex_representation = hex(int(binary_str, scale))[2:].upper()  # Преобразование в шестнадцатеричное представление
 
-    # Добавление нулей в начало, если строка короче 14 символов
-    if len(hex_representation) < 14:
-        hex_representation = '0' * (14 - len(hex_representation)) + hex_representation
+    # Добавление нулей в начало
+    if len(hex_representation) < n and is_needed:
+        hex_representation = '0' * (n - len(hex_representation)) + hex_representation
 
     return hex_representation
 
